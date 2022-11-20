937817de52cfd61eba94fd796a822ff892737317
regina afu <afuda@NAP-NX-LAP-021.CHQ.NNPCGroup.local>
2022-11-19 19:09:12 -0500

    Template fields added

--- src/Templates/ReviewTemplate.java
+++ src/Templates/ReviewTemplate.java
@@ -1,4 +1,8 @@
 package Templates;
 
 public class ReviewTemplate extends Template {
+    private String file;
+    public ReviewTemplate(int templateId, String templateName, String templateDescription) {
+        super(templateId, templateName, templateDescription);
+    }
 }
